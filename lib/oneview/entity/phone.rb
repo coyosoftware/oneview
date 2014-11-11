112d6ecc747193fb4037feee100c84eb5e888a45
Gustavo Berdugo <gustavo@coyo.com.br>
2014-11-11 16:45:52 -0200

    Added Base Entity Class

--- lib/oneview/entity/phone.rb
+++ lib/oneview/entity/phone.rb
@@ -1,17 +1,9 @@
+require 'oneview/entity/base'
+
 module Oneview
   module Entity
-    class Phone
+    class Phone < Base
       attr_accessor :id, :number, :area_code, :country_code
-      
-      def as_parameter
-        variables = instance_variables.map do |name|
-          variable_name = name.to_s.tr("@", "")
-          
-          [variable_name, instance_variable_get(name)]
-        end
-         
-        Hash[variables]
-      end
     end
   end
 end
--- lib/oneview/version.rb
+++ lib/oneview/version.rb
@@ -1,3 +1,3 @@
 module Oneview
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end

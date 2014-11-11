957455f45f25fb3d483a714b23cf723f0613498f
Gustavo Berdugo <gustavo@coyo.com.br>
2014-11-11 16:29:57 -0200

    Configuration to allow codeclimate to work with webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ CodeClimate::TestReporter.start
 require 'oneview'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+
 RSpec.configure do |config|
   config.mock_with :rspec
 end

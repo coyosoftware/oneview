d7705309ad1a25d6f85b7656614f95debf068691
Carlos Diogo <cdiogo.dias@gmail.com>
2016-11-04 10:13:01 -0200

    Add the update method to contacts

--- lib/oneview/api/contacts.rb
+++ lib/oneview/api/contacts.rb
@@ -4,7 +4,7 @@ module Oneview
       require_all 'oneview/entity', 'contact', 'dynamic_field', 'phone'
       
       base_uri "http://www.oneview.com.br/api/contacts"
-      
+
       def create(data)
         return parse_response(self.class.post("/", :body => build_body(data), :headers => header)) if data.is_a?(Hash)
         return parse_response(self.class.post("/", :body => build_body(data.as_parameter), :headers => header)) if data.is_a?(Oneview::Entity::Contact)
@@ -12,6 +12,12 @@ module Oneview
       end
       alias :new :create
       
+      def update(id, data)
+        return parse_response(self.class.patch("/" + id, :body => build_body(data), :headers => header)) if data.is_a?(Hash)
+        return parse_response(self.class.patch("/" + id, :body => build_body(data.as_parameter), :headers => header)) if data.is_a?(Oneview::Entity::Contact)
+        raise ArgumentError
+      end
+
       private
         def build_body(parameters)
           super({:contact => parameters})
